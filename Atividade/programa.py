53b92b666a8ec9f1662e630448ede5c6cd91fe89
VPB <vitorpossebontra@gmail.com>
2024-11-06 21:23:31 -0300

    Programa.py

---
+++ Atividade/programa.py
@@ -0,0 +1,11 @@
+from Biblioteca.rh import Funcionario
+
+fun1 = Funcionario()
+fun1.nome = 'Pedro'
+fun1.departamento = 'TI'
+print(fun1.imprimir_cracha())
+
+fun2 = Funcionario()
+fun2.nome = 'Ana'
+fun2.departamento = 'Adm'
+print(fun2.imprimir_cracha())
